9940fdacd2fd245fa5b9ceadd8dace73d5bcaadf
Sheldon Choo <sheldon.choo@ontariotechu.net>
2021-04-15 14:09:49 -0400

    Completed weekly script

--- Daily/Backend.sh
+++ Daily/Backend.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 chdir ../Backend/out/production/3060ProjectPhase4
 
-java sample.BackEnd ../../../../Daily/BackendComponents/oldMasterBankAccounts.txt ../../../../Daily/BackendComponents/mergedBankAccountTransaction.txt
+java sample.BackEnd ../../../../Daily/BackendComponents/masterBankAccounts.txt ../../../../Daily/BackendComponents/mergedBankAccountTransaction.txt
---
+++ Weekly/weeklyScript.sh
@@ -0,0 +1,31 @@
+#!bin/sh
+
+#Set initial values before iterating over inputs
+cd ..
+rm Daily/BackendComponents/*
+cp Weekly/masterBankAccounts.txt Daily/BackendComponents
+
+# Run the daily script 5 times
+for i in 1 2 3 4 5
+do 
+    #Move .inp files
+    rm Daily/input/*
+    cp Weekly/inputs/day$i/* Daily/input
+
+    #Run the dailyscript
+    cd Daily
+    sh dailyScript.sh
+
+    #Move new master bank accounts into BackEnd dir
+    cd DailyOutputs
+    newMaster=$(find . -name '*newMasterBankAccounts*')
+    cp $newMaster masterBankAccounts.txt
+    cd ..
+    rm BackendComponents/*
+    mv DailyOutputs/masterBankAccounts.txt BackendComponents 
+
+    #Reset for next iteration
+    cd ..
+done
+
+echo Weekly script complete
